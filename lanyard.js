880e7918e92e10602c8b9b200d7c72ce195b81ec
fal <rhbr1109@proton.me>
2025-03-23 05:55:37 +0600

    updated h2p guide, made refreshed blogpost css

--- lanyard.js
+++ lanyard.js
@@ -17,3 +17,23 @@ async function avatar(){
         console.error(error);
     }
 }
+
+async function spotify(){
+
+    try{
+
+        const response = await fetch(`https://api.lanyard.rest/v1/users/719202528130236436`);
+
+        console.log(response)
+
+        const data = await response.json();
+        const albumUrl = `$(data.data.spotify.album_art_url)`;
+        const imgElement = document.getElementById("spotify");
+
+        imgElement.src = albumUrl;
+        imgElement.style.display = 'block';
+    }
+    catch(error){
+        console.error(error);
+    }
+}
