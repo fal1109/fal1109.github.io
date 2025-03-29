264f766e21e22dc92a54a088f1947b82984118ab
fal <rhbr1109@proton.me>
2025-03-29 16:12:55 +0600

    m

--- scripts/tetrio.js
+++ scripts/tetrio.js
@@ -1,13 +1,12 @@
-
 (async function() {
-	const res = await fetch("https://ch.tetr.io/api/users/62550408ecad38a39b7a5bd7/summaries");
+	const res = await fetch("https://api.allorigins.win/raw?url=https://ch.tetr.io/api/users/62550408ecad38a39b7a5bd7/summaries");
 
 	console.log(res);
 
 	const data = await res.json();
 
-	const Sprintpps = `data.data.40l.record.results.aggregatestats.pps`;
-	const Sprinttime = (data.data["40l"].record.results.stats.finaltime / 100).toFixed(3);
+	const Sprintpps = data.data["40l"].record.results.aggregatestats.pps.toFixed(2);
+	const Sprinttime = ((data.data["40l"].record.results.stats.finaltime / 1000)).toFixed(3);
 
 	const SprinttimeElement = document.getElementById("Sprinttime");
 	SprinttimeElement.innerHTML = Sprinttime;
@@ -15,12 +14,12 @@
 	const SprintppsElement = document.getElementById("Sprintpps");
 	SprintppsElement.innerHTML = Sprintpps;
 
-	const Blitzpps = `data.data.blitz.record.results.aggregatestats.pps`;
-	const Blitzscore = `data.data.blitz.record.results.stats.score`;
+	const Blitzpps = data.data.blitz.record.results.aggregatestats.pps.toFixed(2);
+	const Blitzscore = data.data.blitz.record.results.stats.score;
 
-    const BlitzppsElement = document.getElementById("Blitzpps");
-    BlitzppsElement.innerHTML = Blitzpps;
+	const BlitzppsElement = document.getElementById("Blitzpps");
+	BlitzppsElement.innerHTML = Blitzpps;
 
-    const BlitzscoreElement = document.getElementById("Blitzscore");
-    BlitzscoreElement.innerHTML = Blitzscore;
+	const BlitzscoreElement = document.getElementById("Blitzscore");
+	BlitzscoreElement.innerHTML = Blitzscore;
 })();
