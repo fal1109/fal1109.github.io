984d5592384f5b1548e56723464d927b5eba7a73
fal <rhbr1109@proton.me>
2025-03-29 21:15:50 +0600

    tetrio stats added, and i copied mebaddev for it https://mebaddev.net/ for reference

--- scripts/tetrio.js
+++ scripts/tetrio.js
@@ -11,15 +11,30 @@
 	const SprinttimeElement = document.getElementById("Sprinttime");
 	SprinttimeElement.innerHTML = Sprinttime;
 
-	const SprintppsElement = document.getElementById("Sprintpps");
-	SprintppsElement.innerHTML = Sprintpps;
+
 
 	const Blitzpps = data.data.blitz.record.results.aggregatestats.pps.toFixed(2);
 	const Blitzscore = data.data.blitz.record.results.stats.score;
 
-	const BlitzppsElement = document.getElementById("Blitzpps");
-	BlitzppsElement.innerHTML = Blitzpps;
 
 	const BlitzscoreElement = document.getElementById("Blitzscore");
 	BlitzscoreElement.innerHTML = Blitzscore;
+
+
+})();
+
+
+(async function() {
+	const res = await fetch("https://api.allorigins.win/raw?url=https://ch.tetr.io/api/users/62550408ecad38a39b7a5bd7/summaries/league");
+
+	console.log(res);
+
+	const data = await res.json();
+
+	const Country = data.data.standing_local;
+	const CountryElement = document.getElementById("Country");
+	
+	CountryElement.innerHTML = Country;
+
+	
 })();
